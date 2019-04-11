e98b93fb3de996aa893194d40feeee890c78fc0a
lwj <13453335076@163.com>
2019-04-11 11:46:53 +0800

    first

--- src/main/java/cn/bdqn/SLController.java
+++ src/main/java/cn/bdqn/SLController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SLController {
     @RequestMapping("/toIndex")
     public String toIndex(){
-        return "index";
+        return "Login";
     }
+    /*跳转到首页面*/
     @RequestMapping("/toIndex2")
     public String toIndex2(){
-        return "index";
+        return "SystemLoginDefaultPage";
     }
-    @RequestMapping("/toIndex3")
+    /*在首页面点击后台管理跳入用户管理页面*/
+    @RequestMapping("/toUserManagement")
     public String toIndex3(){
-        return "index";
+        return "UserManagement";
     }
     @RequestMapping("/toIndex4")
     public String toIndex4(){
@@ -30,6 +32,6 @@ public class SLController {
     @RequestMapping("/toIndex6")
     public String toIndex6(){
         System.out.println("陈东阳是个狗");
-        return "index";
+        return "AccessManagement";
     }
 }
