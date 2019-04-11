bf952807ba4b03beed8fb10f64a4685b2a80f331
lwj <13453335076@163.com>
2019-04-11 12:17:13 +0800

    first

--- src/main/java/cn/bdqn/BdqnApplication.java
+++ src/main/java/cn/bdqn/BdqnApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BdqnApplication {
+public class BdqnApplication
+{
 
     public static void main(String[] args) {
         SpringApplication.run(BdqnApplication.class, args);
